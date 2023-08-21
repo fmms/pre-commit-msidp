2c676356cf024041eec46b5acb901fabfabd3b3f
Felix Moeller <felix.moeller@noventum.de>
2023-08-21 11:37:57 +0200

    sort the same as tabular editor

--- pre_commit_hooks/bimsort.py
+++ pre_commit_hooks/bimsort.py
@@ -1,13 +1,60 @@
 import argparse, sys
 import json
 
-def bim_is_unsorted(filename):
-    return True
+def sort_bim_json (jsonObject):
+    changed = False
+    if "model" in jsonObject.keys():
+        print ("has a model...")
+        if "dataSources" in jsonObject["model"]:
+            print ("sorting datasources")
+            before = [ds["name"] for ds in jsonObject["model"]["dataSources"]]
+            jsonObject["model"]["dataSources"] = sorted(jsonObject["model"]["dataSources"], key = lambda e: e["name"])
+            after = [ds["name"] for ds in jsonObject["model"]["dataSources"]]
+            if (before != after):
+                print("changed from", before, "to", after, ".")
+                changed = True
+        if "tables" in jsonObject["model"]:
+            print ("sorting tables")
+            before = [ds["name"] for ds in jsonObject["model"]["tables"]]
+            jsonObject["model"]["tables"] = sorted(jsonObject["model"]["tables"], key = lambda e: e["name"])
+            after = [ds["name"] for ds in jsonObject["model"]["tables"]]
+            if (before != after):
+                print("changed from", before, "to", after, ".")
+                changed = True
+            for t in jsonObject["model"]["tables"]:
+                 if "columns" in t.keys() and False: # do not sort columns for now, this is not done by Tabular Editor either
+                    print ("sorting columns of", t["name"])
+                    t["columns"]= sorted(t["columns"], key = lambda e: e["name"])
+        if "relationships" in jsonObject["model"]:
+            print ("sorting relationships")
+            before = [ds["name"] for ds in jsonObject["model"]["relationships"]]
+            jsonObject["model"]["relationships"] = sorted(jsonObject["model"]["relationships"], key = lambda e: e["name"])
+            after = [ds["name"] for ds in jsonObject["model"]["relationships"]]
+            if (before != after):
+                print("changed from", before, "to", after, ".")
+                changed = True
+        if "roles" in jsonObject["model"]:
+            print ("sorting roles")
+            before = [ds["name"] for ds in jsonObject["model"]["roles"]]
+            jsonObject["model"]["roles"] = sorted(jsonObject["model"]["roles"], key = lambda e: e["name"])
+            after = [ds["name"] for ds in jsonObject["model"]["roles"]]
+            if (before != after):
+                print("changed from", before, "to", after, ".")
+                changed = True
+    return changed, jsonObject
 
-def sbim_sort(filename):
+def bim_is_unsorted(filename):
+    with open(filename, mode="rb") as file_processed: 
+        jsonObject = json.load(file_processed)
+        isSorted, trash = sort_bim_json(jsonObject)
+    return isSorted
+
+def bim_sort(filename):
     with open(filename, mode="rb") as file_processed:
-        data = json.dumps(json.load(file_processed), sort_keys=True, indent=2, separators=(',', ': '))
-    with open(filename, mode="w") as outfile:
+        jsonObject = json.load(file_processed)
+        trash, jsonObject = sort_bim_json(jsonObject)
+        data = json.dumps(jsonObject, sort_keys=False, indent=2, separators=(',', ': '), ensure_ascii=False)
+    with open(filename, mode="w", encoding='utf8') as outfile:
         outfile.write(data)
 
 def main(argv=None):
@@ -17,7 +64,7 @@ def main(argv=None):
     unsorted_bims = [f for f in args.filenames if f.endswith('.bim') and bim_is_unsorted(f)]
     for unsorted_bim in unsorted_bims:
         print(f"Sort .bim file: {unsorted_bim}")
-        sbim_sort(unsorted_bim)
+        bim_sort(unsorted_bim)
     if unsorted_bims:
         print("")
         print(".bim files have been sorted. Now aborting the commit.")
